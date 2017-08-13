f8274d125078a24675fc08618952c024eef91c02
taskinosman <osman@taskin.com>
2017-08-13 21:54:28 +0300

    Delete test.pl

--- test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-require InstrumentalApp;
-use Data::Dumper;
-
-
-print Dumper InstrumentalApp::auth("0829a9ba6251151c37035d9e0966dcbc");
-#InstrumentalApp::increment("test.a.b");
-#InstrumentalApp::gauge("test.a.c",555);
