9702017f901e141ef63eaae6aee41310cb572f86
taskinosman <osman@taskin.com>
2017-08-14 07:22:22 +0300

    does not work properly

--- InstrumentalApp.pm
+++ InstrumentalApp.pm
@@ -6,24 +6,27 @@ use IO::Socket::INET;
 use Data::Dumper;
 
 # auto-flush on socket
-$| = 1;
-my $socket;
 
+my $socket;
+my $apikey;
+$| = 1;
 
 sub init() {
+	$| = 1;
 	$socket = new IO::Socket::INET (
 	    PeerHost => 'collector.instrumentalapp.com',
 	    PeerPort => '8000',
 	    Proto => 'tcp',
 	);
 	return undef unless $socket;
-	return InstrumentalApp::send("hello version perl/custom/1.0.0\n");
+	InstrumentalApp::send("hello version perl/custom/1.0.1\n");
+	return InstrumentalApp::send("authenticate $apikey\n");
 }
 
 sub auth() {
 	my ($token)=@_;
-	InstrumentalApp::init() if (!defined($socket)) ;
-	return InstrumentalApp::send("authenticate $token\n");
+	$apikey=$token;
+	InstrumentalApp::init() if (!defined($socket)) ;	
 }
 
 sub increment() {
@@ -46,20 +49,23 @@ sub gauge() {
 sub send() {
 	my ($data)=@_;
 	# data to send to a server
-	#print ">$data";
+	print ">$data";
 	my $size = $socket->send($data);
+	#$socket->flush;
 	# receive a response of up to 1024 characters from server
 	my $response = "";
 	$socket->recv($response, 1024);
-	#print "received response: $response\n";
+	print "received response: $response\n";
 	if ($response=~/ok/mi) { return 1; }
 	return undef;
 }
 
 sub sendOnly() {
 	my ($data)=@_;
-	#print ">$data";
+	print ">$data";
 	my $size = $socket->send($data);
+	print "sent $size bytes\n";
+	$socket->flush;
 	return 1;
 }
 
--- test.pl
+++ test.pl
@@ -6,16 +6,28 @@ print "API Key: ".$ARGV[0]."\n";
 if (defined(InstrumentalApp::auth($ARGV[0]))) {
 	print "Authenticated successfully\n";
 	for(1..9) {
-		InstrumentalApp::increment("test.second");
-		print "Incrementing key test.second\n";
+		InstrumentalApp::increment("test3.second");
+		print "Incrementing key test.second $_\n";
 	}
-	for(1..9) {
-		InstrumentalApp::gauge("test.third",$_);
-		print "Gauging key test.third by $_\n";
-	}
+	# for(1..9) {
+	# 	InstrumentalApp::gauge("test3.third",$_);
+	# 	print "Gauging key test.third by $_\n";
+	# }
+InstrumentalApp::increment("test3.5");
+$total=25;
+while(1) {
+	sleep(1); $total--;
+	print "Sleeping $total left\n";
+	if ($total<0) { last; }
+}
 
+
+InstrumentalApp::increment("test3.forth");
+
+InstrumentalApp::close();
 	print "Finished";
 }
 else {
 	print "Could not authenticate\n";
 }
+
